cc00fb06a657c929040a694c8b1555717036fcf7
tortiluaDev <artemii.webdes@gmail.com>
2025-01-23 23:22:38 +0300

    added data

--- data/products.js
+++ data/products.js
@@ -1,56 +1,74 @@
 const catalogue = {
-	iphones: {
-		'iphone 16': {
+	iphones: [
+		{
+			name: 'iphone 16',
 			price: 75_000,
 			img: '',
 			colors: ['black', 'red', 'blue'],
+			id: '1001',
 		},
-		'iphone 15': {
+		{
+			name: 'iphone 15',
 			price: 63_000,
 			img: '',
 			colors: ['black', 'red', 'white'],
+			id: '1002',
 		},
-		'iphone 14': {
+		{
+			name: 'iphone 16',
 			price: 56_000,
 			img: '',
 			colors: ['pink', 'red', 'white'],
+			id: '1003',
 		},
-	},
+	],
 
-	GPUs: {
-		'nVidia GeForce RTX 4070': {
+	GPUs: [
+		{
+			name: 'nVidia GeForce RTX 4070',
 			price: 70_000,
 			img: '',
 			brands: ['MSI', 'ASUS', 'KFA2'],
+			id: '1101',
 		},
-		'nVidia GeForce RTX 4080': {
+		{
+			name: 'nVidia GeForce RTX 4080',
 			price: 90_000,
 			img: '',
 			brands: ['MSI', 'ASUS', 'KFA2'],
+			id: '1102',
 		},
-		'nVidia GeForce RTX 4060': {
+		{
+			name: 'nVidia GeForce RTX 4060',
 			price: 40_000,
 			img: '',
 			brands: ['PALIT', 'ASUS', 'KFA2'],
+			id: '1103',
 		},
-	},
+	],
 
-	consoles: {
-		'playstation 5': {
+	consoles: [
+		{
+			name: 'playstation 5',
 			price: 51_000,
 			img: '',
 			version: ['slim', 'standard'],
+			id: '1201',
 		},
-		'playstation 5 pro': {
+		{
+			name: 'playstation 5 pro',
 			price: 80_000,
 			img: '',
+			id: '1202',
 		},
-		'playstation 4': {
+		{
+			name: 'playstation 4',
 			price: 35_000,
 			img: '',
 			version: ['slim', 'standard', 'pro'],
+			id: '1203',
 		},
-	},
+	],
 }
 
 module.exports = catalogue
--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express')
-const catalogue = require('./data/goods')
+const bodyParser = require('body-parser')
+const catalogue = require('./data/products')
 
 const app = express()
 const PORT = 5600
 
-app.get((req, res) => {})
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/products', (req, res) => {
+	res.json(catalogue)
+})
+
+app.get('/product/:id', (req, res) => {
+	const id = req.params.id.includes(':')
+		? req.params.id.replace(':', '')
+		: req.params.id
+
+	const allProducts = [...Object.values(catalogue)].flat()
+	const product = allProducts.find(product => product.id === id)
+
+	if (product) {
+		res.json(product)
+	} else {
+		res.status(404).json({ error: 'Product not found' })
+	}
+})
 
 app.listen(PORT, () => console.log(`Server was started on port: ${PORT}`))
